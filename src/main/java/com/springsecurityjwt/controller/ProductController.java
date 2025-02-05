9fa94e39fead3e331dd8c63b53f0f55310a7900c
Manhcodee <phammanh2952@gmail.com>
2025-02-05 21:25:13 +0700

    add controller

---
+++ src/main/java/com/springsecurityjwt/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.springsecurityjwt.controller;
+
+import com.springsecurityjwt.model.Product;
+import com.springsecurityjwt.service.product.IProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+@CrossOrigin("*")
+public class ProductController {
+
+    private final IProductService productService;
+
+    public ProductController(IProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getProduct() {
+        List<Product> products = productService.findAll();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+}
